55e9d7e667e7993543f6cf64244d220633c7660f
Sazanamin <rachel9029@icloud.com>
2020-12-13 05:29:48 +0900

    add Todoアプリ作成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-client';
+
+  todoList: string[] = [];
+  newTodo: string = '';
+
+  public addTodo(): void {
+    if(this.newTodo == '') {
+      return;
+    }
+
+    this.todoList.push(this.newTodo);
+    this.newTodo = '';
+  }
+
+  public deleteTodo(target: number): void {
+    this.todoList.splice(target, 1);
+  }
 }
